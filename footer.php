e10224bb1c4bf0fac80131f9aa298073c4f9d100
Joppe2003 <564285@edu.rocmn.nl>
2023-03-13 13:54:28 +0100

    houses page

---
+++ footer.php
@@ -0,0 +1,3 @@
+<?php
+echo '<footer><p class="bg-white d-flex justify-content-center m-1 p-1">tekst</p></footer></html>';
+?>
--- woningen.php
+++ woningen.php
@@ -1,3 +1,81 @@
 <?php
-echo 'test';
+include 'header.php';
+?>
+    <body class="wv-100 hv-100 bg-light d-flex flex-column">
+    <div class="container mb-auto">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="m-lg-3 mt-3 bg-white">
+                    test
+                </div>
+            </div>
+            <div class="col-lg-9">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="mt-3 m-lg-3">
+                            <div class="card" style="width: 18rem;">
+                                <img src="stock.jfif" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="mt-3 m-lg-3">
+                            <div class="card" style="width: 18rem;">
+                                <img src="stock.jfif" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="mt-3 m-lg-3">
+                            <div class="card" style="width: 18rem;">
+                                <img src="stock.jfif" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="mt-3 m-lg-3">
+                            <div class="card" style="width: 18rem;">
+                                <img src="stock.jfif" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="mt-3 m-lg-3">
+                            <div class="card" style="width: 18rem;">
+                                <img src="stock.jfif" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+<?php
+include 'footer.php';
 ?>
