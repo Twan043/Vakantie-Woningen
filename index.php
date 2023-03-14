8b04e746116b8e6359100890be6bf1e796c66f8c
Joppe2003 <564285@edu.rocmn.nl>
2023-03-14 10:08:23 +0100

    index fix

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include 'header.php';
+?>
+<body class="wv-100 hv-100 bg-light d-flex flex-column">
+<div class="container m-auto" style="text-align: center">
+    <h6>Vakantie woningen</h6>
+</div>
+</body>
+<footer>
+    <div class="bg-white pt-1 pb-1 mt-1 d-flex justify-content-center" style="text-align: center">
+        Copyright: © Alle rechten voor behouden 2021 - <script>document.write(new Date().getFullYear())</script>
+    </div>
+</footer>
+</html>
