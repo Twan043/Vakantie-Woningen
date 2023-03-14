b104904b14666a4f0056ec492eec034ba157e061
Dogukan <564937@edu.rocmn.nl>
2023-03-14 11:45:00 +0100

    Update

--- header.php
+++ header.php
@@ -14,16 +14,16 @@ echo '<!doctype html>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </head>
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
         <div class="container-fluid">
-             <a class="navbar-brand" href="index.html">Home</a>
+             <a class="navbar-brand text-white" href="index.php">Home</a>
              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                  <span class="navbar-toggler-icon"></span>
              </button>
              <div class="collapse navbar-collapse" id="navbarText" style="justify-content: end !important;">
                  <ul class="navbar-nav mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="woningen.php">Woningen</a>
+                        <a class="nav-link text-white" href="woningen.php">Woningen</a>
                     </li>
                  </ul>
             </div>
--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
 include 'header.php';
 ?>
-<body class="wv-100 hv-100 bg-light d-flex flex-column">
+<body class="wv-100 hv-100 bg-primary d-flex flex-column">
 <div class="container m-auto" style="text-align: center">
-    <h6>Vakantie woningen</h6>
+    <h1 class="text-white">Vakantie woningen</h1>
 </div>
 </body>
 <footer>
-    <div class="bg-white pt-1 pb-1 mt-1 d-flex justify-content-center" style="text-align: center">
-        fiets
+    <div class="bg-secondary text-white pt-1 pb-1 mt-1 d-flex justify-content-center" style="text-align: center">
         Copyright: © Alle rechten voor behouden 2021 - <script>document.write(new Date().getFullYear())</script>
     </div>
 </footer>
