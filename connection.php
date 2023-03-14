d96f079cd638b74b106ce68f3f4bea9f1f1a262d
Ali Simon Al Dabbash <562078@edu.rocmn.nl>
2023-03-14 12:56:27 +0100

    config en connection toegevoegd

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+class Connection
+{
+    public function setConnection() {
+        include 'config.php';
+        $con = mysqli_connect($dbhost,$user,$pass,$dbname);
+        // Check connection
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            exit();
+        }
+        return $con;
+    }
+}
