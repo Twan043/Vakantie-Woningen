064d24e0a500bb94de1b42dcde311f88b9d45e20
Joppe20003 <96177194+Joppe20003@users.noreply.github.com>
2023-03-20 14:01:16 +0100

    Add files via upload

---
+++ single-woning.php
@@ -0,0 +1,36 @@
+<?php
+include 'header.php';
+?>
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vakantie woningen</title>
+</head>
+<body>
+<div class="container">
+    <div class="card">
+        <div class="row">
+            <div class="col-lg-3">
+                <img src="stock.jfif">
+            </div>
+            <div class="col-lg-9">
+                <p>test</p>
+                <p>test</p>
+                <p>test</p>
+                <p>test</p>
+                <p>test</p>
+                <p>test</p>
+                <p>test</p>
+                <p>test</p>
+                <p>test</p>
+                <p>test</p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
